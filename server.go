eb959503a5f3b46a6d862a9f29f51568678fc578
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-08-28 16:09:33 -0500

    serving static files

--- server.go
+++ server.go
@@ -3,52 +3,37 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 
-	"database/sql"
+	"html/template"
 
 	"github.com/gofiber/fiber/v2"
-	_ "github.com/lib/pq"
 )
 
-var (
-	Host       = "localhost"
-	PasswdFile = "dbpasswd"
-	Port       = 5432
-	User       = "postgres"
-	Password   = readPassword(PasswdFile)
-	DB         = "test"
-)
-
-func readPassword(name string) string {
-	content, _ := os.ReadFile(name)
-	return string(content)
+type RootContext struct {
+	Title string
 }
 
 func main() {
 	app := fiber.New()
-	conn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		Host, Port, User, Password, DB)
-	db, _ := sql.Open("postgres", conn)
-	defer db.Close()
+	app.Static("/static/", "./public")
 
+	/* Main Page */
 	app.Get("/", func(c *fiber.Ctx) error {
-		/* Main Page */
-		rows, err := db.Query("select name from colors where name = 'red';")
-		defer rows.Close()
+		fmt.Println(c)
+		tmpl, err := template.ParseFiles("templates/index.html")
 		if err != nil {
 			log.Fatal(err)
 		}
-		for rows.Next() {
-			var color string
-			rows.Scan(&color)
-			fmt.Println(color)
-		}
+		c.Context().SetContentType("text/html")
+		tmpl.Execute(c, RootContext{
+			Title: "jackdavidson",
+		})
+
 		return nil
 	})
 
+	/* Get Blog By Title */
 	app.Get("/blogs/:title", func(c *fiber.Ctx) error {
-		/* Get Blog By Title */
 		return nil
 	})
 
