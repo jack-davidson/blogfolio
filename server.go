a8ed5d8fb6c7160ac2986c834c1f662dea65ae38
Jack Davidson <jackmaverickavidson@gmail.com>
2021-08-29 14:59:39 -0500

    update

--- server.go
+++ server.go
@@ -15,9 +15,23 @@ const (
 	Port = 3000
 )
 
-type IndexContext struct {
-	Title string
-	Name  string
+const (
+	Intro = "Hello! I'm Jack Davidson, a student Software Engineer in Austin, Texas. I like playing around with gnu/linux machines and programming in Go."
+	About = `
+<p></p>
+	`
+)
+
+type SiteContext struct {
+	Title   string
+	Name    string
+	Email   string
+	Content SiteContent
+}
+
+type SiteContent struct {
+	Intro template.HTML /* Short introduction. */
+	About template.HTML /* Multi-paragraph about section. */
 }
 
 func main() {
@@ -34,9 +48,14 @@ func main() {
 			log.Fatal(err)
 		}
 		c.Context().SetContentType("text/html")
-		tmpl.Execute(c, IndexContext{
+		tmpl.Execute(c, SiteContext{
 			Title: "jackdavidson.tech",
 			Name:  "Jack Davidson",
+			Email: "jackmaverickdavidson@gmail.com",
+			Content: SiteContent{
+				Intro: template.HTML(Intro),
+				About: template.HTML(About),
+			},
 		})
 
 		return nil
